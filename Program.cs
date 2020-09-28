271a678708f1fc91041990477b6ca6c24907c88e
ErickEW <60982154+ErickEW@users.noreply.github.com>
2020-09-28 14:50:05 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+
+            Console.WriteLine("Inserta un numero");
+
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= Num; i ++)
+            {
+              System.Console.Write("1, ");
+            }
+
+            Console.WriteLine(Num);
+
+        }
+    }
+}
